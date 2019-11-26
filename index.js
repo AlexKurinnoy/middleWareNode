fe8da4f4ded22e84386e74b1794a580d61edede2
unknown <a.n.kurinnoy@gmail.com>
2019-11-26 10:45:53 +0200

    add cms property image upload

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-let bodyParser = require('body-parser')
-let app = express()
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({
+    uploadDir: './img'
+});
+const bodyParser = require("body-parser");
+
+let app = express();
 let port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
@@ -12,8 +17,28 @@ app.use(
 let Users = require("./routes/Users")
 let Articles = require("./routes/Articles")
 
+
 app.use("/users", Users)
 app.use("/articles", Articles)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use('/static', express.static('img'));
+
+
+app.post('/upload', multipartMiddleware, (req, res) => {
+    res.send(
+        {"uploaded": true,
+        "url":'http://localhost:3000/static/'+(req.files.upload.path).substr(4)}
+);
+    console.log(req.files)
+ });
+
+
+
+
 
 app.listen(port, function () {
     console.log("Server is running on port " + port)
--- routes/Articles.js
+++ routes/Articles.js
@@ -2,10 +2,18 @@ const express = require("express")
 const articles = express.Router()
 const cors = require("cors")
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-
+const bcrypt = require("bcrypt") 
+const bodyParser = require("body-parser");
 const Article = require("../models/Article")
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({
+    uploadDir: './img'
+});
 articles.use(cors())
+articles.use(bodyParser.json());
+articles.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 process.env.SECRET_KEY = 'secret'
 
@@ -94,5 +102,12 @@ articles.post('/delete', (req, res) => {
         res.send('error: ' + err)
     })
 })
+
+articles.post('/upload', multipartMiddleware, (req, res) => {
+       res.send(req.files);
+    });
+
+
+
 
 module.exports = articles;
